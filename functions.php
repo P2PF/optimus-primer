33b6e6236308cbdce44e0b11a9fc1426a9668798
Javier Arturo Rodriguez <javier@rodriguez.org.mx>
2017-08-04 11:13:53 +0200

    Initial import

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+// put custom code here
+
+// Add comments to Projects
+
+//add_filter('projectposttype_args', 'custom_filter_project_args');
+//
+//function custom_filter_project_args($args) {
+//	if (isset($args['supports'])) {
+//		$args['supports'][] = 'comments';
+//	}
+//	return $args;
+//}
