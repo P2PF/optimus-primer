7e3ab30ba8ee11c07124c63ba29ecb45246417e9
Javier Arturo Rodriguez <javier@rodriguez.org.mx>
2017-08-05 11:37:38 +0200

    Improved page query. Initial grid layout.

--- page-index.php
+++ page-index.php
@@ -13,15 +13,38 @@ get_header();
         <div class="<?php echo petal_class('content-fullwidth') ?>">
             <?php get_template_part('templates/content-page'); ?>
             <div class="page-grid">
+
                 <?php
-                $pages = get_pages(array('hierarchical' => 0, 'parent' => get_the_ID()));
-                foreach ($pages as $page) {
-                    ?>
-                    <div class="block">
-                        <div class="pg1"><?php the_title()  ?></div>
+                $args = array(
+                    'post_parent' => $post->ID,
+                    'post_type' => 'page',
+                    'orderby' => 'menu_order'
+                );
+
+                $child_query = new WP_Query($args);
+                ?>
+
+                <?php while ($child_query->have_posts()) : $child_query->the_post(); ?>
+                    <div <?php post_class("grid-block"); ?>>
+                        <div class="grid-block-base">
+                        <?php
+                        if (has_post_thumbnail()) {
+                            the_post_thumbnail('page-thumb-mine');
+                        }
+                        ?>
+                        <h3><a href="<?php the_permalink(); ?>" rel="bookmark"
+                               title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+                        </div>
+                        <div class="grid-block-hover">
+                            <a class="btn" href="<?php the_permalink(); ?>">Read more</a>
+                        </div>
                     </div>
-                <?php } ?>
-                <pre><?php print_r($pages) ?></pre>
+                <?php endwhile; ?>
+
+                <?php
+                wp_reset_postdata();
+                ?>
+
             </div>
         </div>
     </div>
