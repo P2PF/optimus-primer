8f039e527293e517a6235712ed975426c9ee15af
Javier Arturo Rodriguez <javier@rodriguez.org.mx>
2017-08-19 17:00:58 +0200

    Added links to footer logos.

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 <?php get_template_part( 'templates/footer' ); ?>
 <footer>
 <?php
-$img = get_stylesheet_directory_uri() . '/public/img';
+    $img = get_stylesheet_directory_uri() . '/public/img';
 ?>
-    <img id="heinrich" src="<?php echo $img ?>/heinrich-boll-stiftung.png">
-    <img id="p2p-footer" src="<?php echo $img ?>/p2pf-footer.png">
-    <img id="cc" src="<?php echo $img ?>/cc-by-sa.png" align="right">
+    <a href="https://www.boell.de/en" target="_blank"><img id="heinrich" src="<?php echo $img ?>/heinrich-boll-stiftung.png"></a>
+    <a href="https://p2pfoundation.net/" target="_blank"><img id="p2p-footer" src="<?php echo $img ?>/p2pf-footer.png"></a>
+    <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank"><img id="cc" src="<?php echo $img ?>/cc-by-sa.png" align="right"></a>
 </footer>
 <?php wp_footer(); ?>
 </body>
