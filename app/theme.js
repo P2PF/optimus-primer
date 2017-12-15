7ddd8bd051f0d032b4596f0a1ed3904d4a14ae15
Javier Arturo Rodriguez <javier@rodriguez.org.mx>
2017-12-15 06:11:22 -0600

    Apply home link hack to breadcrumb. Fix h1 line height (former copy+paste from inVision).

--- app/theme.js
+++ app/theme.js
@@ -1,6 +1,6 @@
 /* Theme-wide initialization */
 var eh=require('equalheight');
 jQuery(function(){
-    jQuery('.title-logo a').attr({href:'/#home'});
+    jQuery('.title-logo a,a[rel="home"]').attr({href:'/#home'});
     eh.equalheight('.equalheight');
 });
